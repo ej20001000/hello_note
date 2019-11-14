65e62f4c61280ed6e3afcd63ea65d04db8bbc5a7
ej20001000 <ejayyj1225@gmail.com>
2019-11-14 17:51:39 +0900

    a

--- HelloNote/src/co/hello/test/WebCrawlingTest.java
+++ HelloNote/src/co/hello/test/WebCrawlingTest.java
@@ -9,9 +9,16 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class WebCrawlingTest {
+
+	Document d = null;
+	Scanner in = new Scanner(System.in);
+
 	public static void main(String[] args) {
-		Document d = null;
-		Scanner in = new Scanner(System.in);
+		WebCrawlingTest crawl = new WebCrawlingTest();
+		crawl.news();
+	}
+
+	public void mange() {
 		while (true) {
 			try {
 				String foodName = in.nextLine();
@@ -30,5 +37,21 @@ public class WebCrawlingTest {
 				}
 			}
 		}
+	}
+
+	public void news() {
+		try {
+			d = Jsoup.connect("http://www.itnews.or.kr/").timeout(6000).get();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		Element ele = d.select("div.td-block-span6").get(1);
+
+		String html=ele.outerHtml().replace("'", "\"");
+		System.out.println(html);
+		
+//		for (Element element : ele) {
+//			System.out.println(element.outerHtml());
+//		}
 	}
 }
